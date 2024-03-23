b01ca88ea48d64b43c114926e4f7f047ddf434a6
Shashi535357227 <149998207+Shashi535357227@users.noreply.github.com>
2024-03-23 20:25:18 +0530

    Update logs.py

--- AltBots/modules/logs.py
+++ AltBots/modules/logs.py
@@ -42,7 +42,7 @@ async def logs(legend):
         fetch = await legend.reply(f"__Fetching Logs...__")
     
         with open("AltLogs.txt", "w") as logfile:
-            logfile.write("⚡🇩𝜩Ⓐ︎🇩 ⚡ [ Bot Logs ]\n\n" + logs)
+            logfile.write("SUPRA [ Bot Logs ]\n\n" + logs)
 
         end = datetime.now()
         ms = (end-start).seconds
